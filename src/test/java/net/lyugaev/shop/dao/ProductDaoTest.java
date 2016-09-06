236ba5cc71bfbded96bc4932e589582c3cf566cb
Dmitry <lyugaev@gmail.com>
2016-09-07 00:07:22 +0200

    !

--- src/test/java/net/lyugaev/shop/dao/ProductDaoTest.java
+++ src/test/java/net/lyugaev/shop/dao/ProductDaoTest.java
@@ -1,11 +1,15 @@
 package net.lyugaev.shop.dao;
 
+//import net.lyugaev.shop.configuration.AppConfig;
 import net.lyugaev.shop.entity.Product;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.junit.Before;
 import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.expect;
@@ -13,6 +17,9 @@ import static org.easymock.EasyMock.expect;
 /**
  * Created by dmitry on 05.09.16.
  */
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = {AppConfig.class})
 public class ProductDaoTest {
 
     private ProductDao productDao;
@@ -30,7 +37,7 @@ public class ProductDaoTest {
 
     @Test
     public void findById() {
-        productDao.findById(3);
+        //productDao.findById(3);
 
 
     }
